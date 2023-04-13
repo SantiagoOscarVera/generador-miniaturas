20ee1fdebb7e1b81b00974d6b1bc7473b438dddb
Puchoanimal <santiagovera280@gmail.com>
2023-04-13 20:19:12 -0300

    logo

--- src/components/auth0/profile.tsx
+++ src/components/auth0/profile.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Box, Grid, Typography } from "@material-ui/core";
+import { Box, Grid, Typography, useMediaQuery } from "@material-ui/core";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
+  const isSmallScreen = useMediaQuery("(max-width: 1000px)");
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -13,9 +14,11 @@ const Profile = () => {
     <React.Fragment>
       {isAuthenticated && (
         <Grid container spacing={1} alignItems="center">
-          <Grid item>
-            {user?.name && <Typography> ¡Bienvenido/a {user.name}!</Typography>}
-          </Grid>
+          {!isSmallScreen && user?.name && (
+            <Grid item>
+              <Typography> ¡Bienvenido/a {user.name}!</Typography>
+            </Grid>
+          )}
           <Grid item>
             {user?.picture && (
               <Box
@@ -46,3 +49,4 @@ const Profile = () => {
 };
 
 export default Profile;
+
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,77 @@
-import React  from "react";
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  FormControlLabel,
+  Switch,
+  Grid,
+} from "@material-ui/core";
+import { useAuth0 } from "@auth0/auth0-react";
+import { LoginButton } from "./auth0/login";
+import { LogoutButton } from "./auth0/logout";
+import Profile from "./auth0/profile";
+import { Link } from "react-router-dom";
+
+interface NavbarProps {
+  checked: boolean;
+  onChange: () => void;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ checked, onChange }) => {
+  const { isAuthenticated } = useAuth0();
+  return (
+    <AppBar style={{ backgroundColor: "#dd2c00", height: "40px" }}>
+      <Toolbar style={{ minHeight: "40px" }}>
+        <Grid container justifyContent="space-between" alignItems="center">
+          <Grid item xs={6} sm={4} style={{ justifyContent: "flex-start" }}>
+            <Link
+              to="/"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <Typography variant={window.innerWidth < 600 ? "subtitle2" : "h6"}>
+                Thumbnail Generator
+              </Typography>
+            </Link>
+          </Grid>
+          <Grid item xs={6} sm={4} style={{ justifyContent: "center" }}>
+            <FormControlLabel
+              control={<Switch checked={checked} onChange={onChange} />}
+              label="Activar vista previa"
+            />
+          </Grid>
+          <Grid item xs={6} sm={4} container>
+  <Grid container direction="row-reverse" alignItems="center" spacing={1}>
+    {isAuthenticated && (
+      <>
+        <Grid item>
+          <LogoutButton />
+        </Grid>
+        <Grid item>
+          <Profile />
+        </Grid>
+      </>
+    )}
+    {!isAuthenticated && (
+      <Grid item>
+        <LoginButton />
+      </Grid>
+    )}
+  </Grid>
+</Grid>
+
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
+
+
+
+/* import React  from "react";
 import { AppBar, Toolbar, Typography, Box, FormControlLabel, Switch} from "@material-ui/core";
 import { useAuth0 } from "@auth0/auth0-react";
 import { LoginButton } from "./auth0/login";
@@ -41,5 +114,5 @@ const Navbar: React.FC<NavbarProps> = ({ checked, onChange }) => {
   );
 };
 
-export default Navbar;
+export default Navbar; */
 
